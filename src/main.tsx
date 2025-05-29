437203d66ace3a846c2fc32b69c36422a5f164d5
MuriloZague <zaguemurilo7@gmail.com>
2025-05-28 21:22:16 -0300

    fix: import Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './components/home'
+import Home from './components/Home'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(

9d002af1cf29aebfd89927733284fff9fc3d772f
MuriloZague <zaguemurilo7@gmail.com>
2025-05-31 20:51:15 -0300

    fix: order calcs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center gap-3 w-[45%] mt-3">
         <h1 className="text-[2.5rem] font-medium text-center max-w-[65%]">Calculadora de Tamanho da Amostra</h1>
-        <Amostra title="Calculadora de Tamanho de Amostra por Média" primeiro="Grau de confiança (z)" segundo="p" terceiro="1 - p" quarto="erro (Ep)" />
         <Amostra title="Calculadora de Tamanho de Amostra por Proporção" primeiro="Grau de confiança (z)" segundo="Desvio Padrão" terceiro="Erro (Em)" quarto="" />
+        <Amostra title="Calculadora de Tamanho de Amostra por Média" primeiro="Grau de confiança (z)" segundo="p" terceiro="1 - p" quarto="erro (Ep)" />
       </div>
     </section>
   )
